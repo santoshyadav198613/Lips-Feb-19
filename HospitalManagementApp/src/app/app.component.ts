117ab717d4c609b7882361785d13b639c2f762b2
santosh yadav <santosh.yadav198613@gmail.com>
2019-02-10 13:37:57 +0530

    aaded script and css

--- HospitalManagementApp/src/app/app.component.ts
+++ HospitalManagementApp/src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // template: `Hello 
+  // {{title}}`,
+  styleUrls: ['./app.component.css'],
+  // styles: ['h1 { color: steelblue }']
 })
 export class AppComponent {
   title = 'HospitalManagementApp';
