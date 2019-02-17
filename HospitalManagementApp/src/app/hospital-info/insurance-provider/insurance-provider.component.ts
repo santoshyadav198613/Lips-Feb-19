1699ed1a5568514ce2ff3f1e5c4c54363655c061
santosh yadav <santosh.yadav198613@gmail.com>
2019-02-17 12:23:46 +0530

    added nwSwitch example

--- HospitalManagementApp/src/app/hospital-info/insurance-provider/insurance-provider.component.ts
+++ HospitalManagementApp/src/app/hospital-info/insurance-provider/insurance-provider.component.ts
@@ -9,11 +9,17 @@ export class InsuranceProviderComponent implements OnInit {
   providers: Array<IInsuranceProviders> = [
     { name: 'BOI', cashLess: false, dailyLimit: 5000 },
     { name: 'Star Health', cashLess: true, dailyLimit: 10000 },
-    { name: 'HDFC', cashLess: true, dailyLimit: 7000 }
+    { name: 'HDFC', cashLess: true, dailyLimit: 7000 },
+    { name: 'UBI', cashLess: true, dailyLimit: 5000 },
+    { name: 'Kotak', cashLess: true, dailyLimit: 8000 }
   ];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  trackByFn(index, item){
+    return index;
   }
 
 }
